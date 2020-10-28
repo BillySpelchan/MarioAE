066c533ed2b3e62ff740d87a0a8821dd2bcdef1b
Billy D. Spelchan <spelchan@gmail.com>
2020-10-28 12:50:22 -0700

    LiteTux generator reworked for full tile prediction.

--- litetux/LTGenerator.py
+++ litetux/LTGenerator.py
@@ -1,44 +1,66 @@
-from litetux import LTMap, LTModel
+from litetux import LTMap, LTModel, FullTileTesting
+import numpy as np
 import random
 
 class LTMapGenerator():
-    def __init__(self):
+    def __init__(self, model=None, cleaner=None, slice_cols=4, slice_rows=14):
         self.map = None
-        self.model = None
-        self.cleaner = None
+        self.model = model
+        if model is None:
+            self.model = FullTileTesting.BitplainEncoder(slice_cols,slice_rows)
+        self.cleaner = cleaner
+        if cleaner is None:
+            self.cleaner = FullTileTesting.BitplainEncoder(slice_cols, slice_rows)
         self.tester = LTModel.PerformTests()
         self.cols_in_slice = 4
-        self.start_slice = None
+        self.start_slice = self.model.encode_slice
 
-    def create_model(self, training, cols_in_slice, hidden=-1, encode=-1, epoch=150):
-        training_set = self.tester.build_training_set(training, cols_in_slice, None)
+    def get_input_output_sets_for_model(self, level_list):
+        ltm = LTMap.LiteTuxMap(1, 1)
+        step_size = self.model.cols
+        in_set = None
+        out_set = None
+        for lvl in level_list:
+            ltm.load(lvl)
+            slice_set = None
+            end_col = ltm.width - self.model.cols
+            for c in range(0, end_col, step_size):
+                if slice_set is None:
+                    slice_set = np.reshape(self.model.encode_slice(ltm, c), (1, self.model.in_nodes))
+                else:
+                    slice_set = np.vstack([slice_set, np.reshape(self.model.encode_slice(ltm, c), (1, self.model.in_nodes))])
+            if in_set is None:
+                in_set = slice_set[0:-1, :]
+                out_set = slice_set[1:, :]
+            else:
+                in_set = np.vstack([in_set, slice_set[0:-1, :]])
+                out_set = np.vstack([out_set, slice_set[1:, :]])
 
-        self.cols_in_slice = cols_in_slice
-        n = training_set.shape[0]
-        self.start_slice = training_set[0]
-        self.model = LTModel.LTModel()
-        hl = hidden if hidden > 0 else training_set.shape[1]//2
-        el = encode if encode > 0 else training_set.shape[1]//4
-        self.model.create_model(training_set.shape[1], hl, el)
-        self.model.train_for_different_output(training_set[:n-cols_in_slice,:], training_set[cols_in_slice:,:],epoch)
+        return (in_set, out_set)
 
-        self.cleaner = LTModel.LTModel()
-        self.cleaner.create_model(training_set.shape[1], hl, el)
-        self.cleaner.train_model(training_set,epoch)
+    def train_models(self, training_levels, epochs = 300):
+        train_in, train_out = self.get_input_output_sets_for_model(training_levels)
+        self.start_slice = train_in[0,:]
+        self.model.train(train_in, train_out, epochs)
+        clean_in = self.cleaner.build_slice_set(training_levels)
+        self.cleaner.train(clean_in, epoch=epochs)
 
     def build_map(self, cols, noise = .1, clean = True):
-        prediction = self.model.predict_slice(self.start_slice)
+        # TODO handle None start slice
+        predicted_map = LTMap.LiteTuxMap(cols, self.model.rows)
+        prediction = self.model.predict(self.start_slice)
         cur_col = 0
         while (cur_col < cols):
-            cur_col += self.cols_in_slice
-            print(self.tester.compare_env_encoding_to_col_string(prediction[0], prediction[0], int(prediction.shape[1]//self.cols_in_slice)))
+            #print(self.tester.compare_env_encoding_to_col_string(prediction[0], prediction[0], int(prediction.shape[1]//self.cols_in_slice)))
             for i in range(prediction.shape[1]):
                 if random.random() < noise:
                     prediction[0,i] = 1 if prediction[0,1] < .5 else 0
-            prediction = self.model.predict_slice(prediction)
+            prediction = self.model.predict(prediction)
             if clean:
-                prediction = self.cleaner.predict_slice(prediction)
-
+                prediction = self.cleaner.predict(prediction)
+            self.model.decode_slice(prediction[0], predicted_map, cur_col)
+            cur_col += self.cols_in_slice
+        print(predicted_map.to_vertical_string())
 
 TRAIN_LEVELS = ["levels/mario-1-1.json",
               "levels/mario-2-1.json","levels/mario-3-1.json",
@@ -52,5 +74,5 @@ EXTRACTORS = [None, LTMap.BasicExtractor, LTMap.BestReachableAsBitsExtractor,
               LTMap.PathExtractor, LTMap.PathExtractorWithJumpState]
 
 gen = LTMapGenerator()
-gen.create_model(TRAIN_LEVELS, 4)
-gen.build_map(80)
+gen.train_models(TRAIN_LEVELS, 4)
+gen.build_map(80, .1, False)
